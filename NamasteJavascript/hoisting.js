50b6d41d4bfc03dfefacbfdf8e99fd6c3b844d1c
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-06-05 07:22:48 +0530

    hoisting

--- NamasteJavascript/hoisting.js
+++ NamasteJavascript/hoisting.js
@@ -1,4 +1,9 @@
 getName();
+
+console.log(getName2);
+
+getName2();
+
 console.log(x);
 
 var x = 7;
@@ -6,3 +11,5 @@ var x = 7;
 function getName() {
   console.log("Namaste Javascript");
 }
+
+var getName2 = () => console.log(x);
