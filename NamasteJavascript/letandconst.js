baa0d723fbc45b34cf576c481c93a113d0bbe4f2
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-06-07 13:07:22 +0530

    let and const

---
+++ NamasteJavascript/letandconst.js
@@ -0,0 +1,29 @@
+console.log("a : ", a);
+// console.log("b : ", b);
+// console.log("c : ", c);
+
+var a = 10;
+let b = 100;
+const c = 1000;
+console.log("b : ", b);
+console.log("c : ", c);
+
+console.log("window.a : ", window.a);
+console.log("window.b : ", window.b);
+console.log("window.c : ", window.c);
+
+console.log("this.a : ", this.a);
+console.log("this.b : ", this.b);
+console.log("this.c : ", this.c);
+
+var a = 10000;
+// let b = 100000;
+// const c = 100000;
+
+let d;
+d = 100000000;
+console.log("d : ", d);
+
+// const e;
+// console.log("e : ", e);
+c = 100000;
