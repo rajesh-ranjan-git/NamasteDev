9b71826af3ca5b82f2affefecac4bee9864472b9
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-06-07 10:13:10 +0530

    undefined vs not defined

---
+++ NamasteJavascript/undefinedvsnotdefined.js
@@ -0,0 +1,24 @@
+console.log("a : ", a);
+
+var a;
+
+if (a === undefined) {
+  console.log("a is undefined");
+} else {
+  console.log("a is not undefined");
+}
+
+a = 7;
+
+if (a === undefined) {
+  console.log("a is undefined");
+} else {
+  console.log("a is not undefined");
+}
+
+a = "Namaste Javascript";
+console.log("a : ", a);
+
+a = undefined;
+console.log("a : ", a);
+console.log("It is very much not recommended to do this.");
