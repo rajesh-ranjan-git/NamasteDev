071429d9fd83d95ccec99eb45f2e84432a606196
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-07-13 09:32:19 +0530

    this keyword

--- NamasteJavascript/Season2/thisKeyword.js
+++ NamasteJavascript/Season2/thisKeyword.js
@@ -1,7 +1,7 @@
 "use strict"; // to run this javascript file in strict mode
 
 // this in global space
-console.log(this); // global object, window in browsers, global in nodejs and can be different for different JS engines wherever we run this javascript (SpiderMonkey or chakra)
+// console.log(this); // global object, window in browsers, global in nodejs and can be different for different JS engines wherever we run this javascript (SpiderMonkey or chakra)
 
 // this inside a function
 function x() {
@@ -11,15 +11,64 @@ function x() {
 // this in strict mode - (this substitution - if the value of this keyword is undefined or null, it will be replaced with global object only in non-strict mode)
 
 // this value depends on how this is called (window)
-x(); // undefined - when called without any reference
-window.x(); // window -- when called with window reference
+// x(); // undefined - when called without any reference
+// window.x(); // window -- when called with window reference
 
 // this inside an object's method
 
-// call, apply, and bind methods (sharing methods)
+const obj1 = {
+  a: 10,
+  b: function () {
+    console.log(this);
+  },
+};
 
-// this inside arrow functions
+// obj1.b();
+
+// call, apply, and bind methods (sharing methods)
+const person1 = {
+  name: "Rajesh",
+  getName: function () {
+    console.log(this.name);
+  },
+};
+
+// person1.getName();
+
+const person2 = {
+  name: "Sayantanee",
+};
+
+// person1.getName.call(person2); // this keyword reference to the object that is calling the function : person2
+
+// this inside arrow functions - arrow function does not its own this bind and it points to its lexical context
+const c = () => {
+  console.log(this);
+};
+
+// c();
+
+const obj2 = {
+  d: 20,
+  e: () => {
+    console.log(this);
+  },
+};
+
+obj2.e(); // it will point to window instead of obj2 (lexical context)
 
 // this inside nested arrow functions
+const obj3 = {
+  d: 30,
+  e: function () {
+    const f = () => {
+      console.log(this); // points to lexical context which is obj3
+    };
 
-// this inside DOM
+    f();
+  },
+};
+
+obj3.e();
+
+// this inside DOM - points to the HTML element that calls it
