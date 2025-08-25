5dd9516455c3b9b5c09895e0411a1db86eea0e6d
Rajesh Ranjan <rajesh.ranjan@aajtak.com>
2025-08-25 10:45:00 +0530

    creating express server

---
+++ NamasteNodeJS/Season2/expressServer/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.use((req, res) => {
+    res.send({
+        status : "ok",
+        message: "Server is running...!"
+    })
+})
+
+app.listen(7777, () => {
+  console.log("Server is listening at post : 7777");
+});
